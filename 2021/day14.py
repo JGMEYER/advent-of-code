915ae9f5335230dfe3216a2087b1fee81a4c3fd7
JGMEYER <meyer.justian@gmail.com>
2021-12-14 16:16:00 -0500

    2021 14 Part 2 (revised algo)

--- 2021/day14.py
+++ 2021/day14.py
@@ -1,24 +1,53 @@
+import math
 import re
 from collections import defaultdict
+from typing import Dict
 
 from common.input import auto_read_input
 
 
-def insert_rules(polymer_template, rules_map):
-    result_polymer = ""
+def _letter_counts_from_polymer_map(polymer_map: Dict[str, int]):
+    """Convert map of character pairs to individual character counts"""
     letter_counts = defaultdict(int)
+    for pair, count in polymer_map.items():
+        left, right = pair
+        letter_counts[left] += count
+        letter_counts[right] += count
+    # Use ceil here to account for first and last characters in a template.
+    # These would result in odd valued counts because they do not belong to two
+    # pairs. Round them up.
+    return {
+        pair: math.ceil(count / 2) for pair, count in letter_counts.items()
+    }
 
-    for idx in range(len(polymer_template)):
-        result_polymer += polymer_template[idx]
-        letter_counts[polymer_template[idx]] += 1
 
-        if idx < len(polymer_template) - 1:
-            left, right = polymer_template[idx : idx + 2]
-            chr_repl = rules_map[left].get(right) if left in rules_map else ""
-            result_polymer += chr_repl
-            letter_counts[chr_repl] += 1
+def step_optimized(polymer_template, rules_map, n_steps):
+    """Step template n times"""
 
-    return result_polymer, letter_counts
+    # Map character pairs to their counts
+    polymer_map = defaultdict(int)
+
+    # Initial setup
+    for idx in range(len(polymer_template) - 1):
+        pair = polymer_template[idx : idx + 2]
+        polymer_map[pair] += 1
+
+    for _ in range(n_steps):
+        new_polymer_map = defaultdict(int)
+        for pair in polymer_map.keys():
+            left, right = pair
+            # Find character to place between left and right
+            chr_repl = (
+                rules_map[left].get(right) if left in rules_map else None
+            )
+            if chr_repl:
+                # Adding the new character splits the pair into two more
+                new_polymer_map[left + chr_repl] += polymer_map[pair]
+                new_polymer_map[chr_repl + right] += polymer_map[pair]
+        polymer_map = new_polymer_map
+
+    letter_counts = _letter_counts_from_polymer_map(polymer_map)
+    return letter_counts
 
 
 def _parse_input(lines):
@@ -36,20 +65,13 @@ def _parse_input(lines):
 
 def part1():
     polymer, rules_map = _parse_input(auto_read_input())
-
-    for _ in range(10):
-        polymer, letter_counts = insert_rules(polymer, rules_map)
-
+    letter_counts = step_optimized(polymer, rules_map, 10)
     return max(letter_counts.values()) - min(letter_counts.values())
 
 
 def part2():
     polymer, rules_map = _parse_input(auto_read_input())
-
-    for day_idx in range(40):
-        print(day_idx)
-        polymer, letter_counts = insert_rules(polymer, rules_map)
-
+    letter_counts = step_optimized(polymer, rules_map, 40)
     return max(letter_counts.values()) - min(letter_counts.values())
 
 
--- 2021/tests/test_day14.py
+++ 2021/tests/test_day14.py
@@ -1,5 +1,9 @@
 from re import I
-from day14 import _parse_input, insert_rules
+from day14 import (
+    _parse_input,
+    step_optimized,
+    _letter_counts_from_polymer_map,
+)
 from common.input import auto_read_input
 
 
@@ -37,34 +41,35 @@ def test__parse_input():
     assert rules_map == expected_rules_map
 
 
-def test_insert_rules():
+def test_step_optimized():
     polymer_template, rules_map = _parse_input(auto_read_input())
-
-    polymer, _ = insert_rules(polymer_template, rules_map)
-    expected_polymer = "NCNBCHB"
-    assert polymer == expected_polymer
-
-    polymer, _ = insert_rules(polymer, rules_map)
-    expected_polymer = "NBCCNBBBCBHCB"
-    assert polymer == expected_polymer
-
-    polymer, _ = insert_rules(polymer, rules_map)
-    expected_polymer = "NBBBCNCCNBBNBNBBCHBHHBCHB"
-    assert polymer == expected_polymer
-
-    polymer, _ = insert_rules(polymer, rules_map)
-    expected_polymer = "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB"
-    assert polymer == expected_polymer
-
-    polymer, _ = insert_rules(polymer, rules_map)
-    assert len(polymer) == 97
-
-    polymer, _ = insert_rules(polymer, rules_map)
-    polymer, _ = insert_rules(polymer, rules_map)
-    polymer, _ = insert_rules(polymer, rules_map)
-    polymer, _ = insert_rules(polymer, rules_map)
-    polymer, letter_counts = insert_rules(polymer, rules_map)
-
+    letter_counts = step_optimized(polymer_template, rules_map, 10)
     expected_letter_counts = {"B": 1749, "C": 298, "H": 161, "N": 865}
-    assert len(polymer) == 3073
+    assert letter_counts == expected_letter_counts
+
+    polymer_template, rules_map = _parse_input(auto_read_input())
+    letter_counts = step_optimized(polymer_template, rules_map, 40)
+    assert letter_counts["B"] == 2192039569602
+    assert letter_counts["H"] == 3849876073
+
+
+def test__letter_counts_from_polymer_map():
+    # Taken from step 2 of example
+    polymer_map = {
+        "NB": 2,
+        "BC": 2,
+        "CC": 1,
+        "CN": 1,
+        "BB": 2,
+        "CB": 2,
+        "BH": 1,
+        "HC": 1,
+    }
+    letter_counts = _letter_counts_from_polymer_map(polymer_map)
+    expected_letter_counts = {
+        "N": 2,
+        "B": 6,
+        "C": 4,
+        "H": 1,
+    }
     assert letter_counts == expected_letter_counts
