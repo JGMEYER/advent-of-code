b29077417d7f2d730d639c0845ce1bcc94d82667
JGMEYER <meyer.justian@gmail.com>
2021-12-18 17:11:53 -0500

    Working _explode()

--- 2021/day18.py
+++ 2021/day18.py
@@ -1,215 +1,226 @@
-# import re
-# from common.input import auto_read_input
+import re
+from common.input import auto_read_input
 
 
-# HEAD_NODE = "^"
-# EMPTY_NODE = "@"
+HEAD_NODE = "^"
+EMPTY_NODE = "@"
 
 
-# class SnailNum(dict):
-#     """Binary tree to represent a snail number.
+class SnailNum(dict):
+    """Binary tree to represent a snail number.
 
-#     It's a binary tree stored as a heap in a dict to keep it easily indexable
-#     and expandable.
-#     """
+    It's a binary tree stored as a heap in a dict to keep it easily indexable
+    and expandable.
+    """
 
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self[0] = HEAD_NODE
+    ORDER_PREORDER = -1
+    ORDER_INORDER = 0
+    ORDER_POSTORDER = 1
 
-#     def __repr__(self):
-#         keys = self.keys()
-#         arr = [None] * (max(keys) + 1)
-#         for idx in keys:
-#             arr[idx] = self.get(idx)
-#         return str(arr)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self[0] = HEAD_NODE
 
-#     @classmethod
-#     def from_list(cls, l):
-#         d = {}
-#         for idx, val in enumerate(l):
-#             d[idx] = val
-#         return SnailNum(d)
+    def __repr__(self):
+        keys = self.keys()
+        arr = [None] * (max(keys) + 1)
+        for idx in keys:
+            arr[idx] = self.get(idx)
+        return str(arr)
 
-#     @classmethod
-#     def get_left_idx(cls, idx):
-#         return (2 * idx) + 1
+    @classmethod
+    def from_list(cls, l):
+        d = {}
+        for idx, val in enumerate(l):
+            d[idx] = val
+        return SnailNum(d)
 
-#     @classmethod
-#     def get_right_idx(cls, idx):
-#         return (2 * idx) + 2
+    @classmethod
+    def get_left_idx(cls, idx):
+        return (2 * idx) + 1
 
-#     @classmethod
-#     def get_parent_idx(cls, idx):
-#         return (idx - 1) // 2
+    @classmethod
+    def get_right_idx(cls, idx):
+        return (2 * idx) + 2
 
-#     def get_left(self, idx):
-#         left_idx = SnailNum.get_left_idx(idx)
-#         return left_idx, self[left_idx]
+    @classmethod
+    def get_parent_idx(cls, idx):
+        return (idx - 1) // 2
 
-#     def get_right(self, idx):
-#         right_idx = SnailNum.get_right_idx(idx)
-#         return right_idx, self[right_idx]
+    def get_left(self, idx):
+        left_idx = SnailNum.get_left_idx(idx)
+        return left_idx, self.get(left_idx)
 
-#     def get_parent(self, idx):
-#         parent_idx = SnailNum.get_parent_idx(idx)
-#         return parent_idx, self[parent_idx]
+    def get_right(self, idx):
+        right_idx = SnailNum.get_right_idx(idx)
+        return right_idx, self.get(right_idx)
 
-#     def _indexes_perorder(self):
-#         pass
+    def get_parent(self, idx):
+        parent_idx = SnailNum.get_parent_idx(idx)
+        return parent_idx, self.get(parent_idx)
 
-#     def indexes_preorder(self):
-#         pass
+    def _foreach(self, func, order, cur_idx=0):
+        cur = self.get(cur_idx)
 
-#     def _magnitude(self, idx):
-#         left_idx = self.get_left_idx(idx)
-#         right_idx = self.get_right_idx(idx)
+        if cur is None:
+            return
 
-#         left = self.get(left_idx)
-#         right = self.get(right_idx)
+        left_idx, left = self.get_left(cur_idx)
+        right_idx, right = self.get_right(cur_idx)
 
-#         if left is not None and right is not None:
-#             return 3 * self._magnitude(left_idx) + 2 * self._magnitude(
-#                 right_idx
-#             )
-#         elif left is None and right is None:
-#             return self[idx]
-#         else:
-#             raise ValueError(
-#                 f"SnailNum does not contain a pair at parent {idx}, left {left_idx}, right {right_idx}"
-#             )
+        if order == SnailNum.ORDER_PREORDER:
+            func(cur_idx, cur)
+            self._foreach(func, order, left_idx)
+            self._foreach(func, order, right_idx)
+        elif order == SnailNum.ORDER_INORDER:
+            self._foreach(func, order, left_idx)
+            func(cur_idx, cur)
+            self._foreach(func, order, right_idx)
+        elif order == SnailNum.ORDER_POSTORDER:
+            self._foreach(func, order, left_idx)
+            self._foreach(func, order, right_idx)
+            func(cur_idx, cur)
 
-#     def magnitude(self):
-#         return self._magnitude(0)
+    def foreach(self, func, order: int):
+        self._foreach(func, order)
 
-#     @classmethod
-#     def _merge(cls, a, b):
-#         # NOTE: Making this a heap made merging unnecessarily complex
-#         result = SnailNum()
+    def _magnitude(self, idx):
+        # TODO cleanup to use get_left and get_right
+        left_idx = self.get_left_idx(idx)
+        right_idx = self.get_right_idx(idx)
 
-#         def _traverse(num, num_idx, result, result_idx):
-#             num_left_idx = num.get_left_idx(num_idx)
-#             num_right_idx = num.get_right_idx(num_idx)
+        left = self.get(left_idx)
+        right = self.get(right_idx)
 
-#             result_left_idx = result.get_left_idx(result_idx)
-#             result_right_idx = result.get_right_idx(result_idx)
+        if left is not None and right is not None:
+            return 3 * self._magnitude(left_idx) + 2 * self._magnitude(
+                right_idx
+            )
+        elif left is None and right is None:
+            return self[idx]
+        else:
+            raise ValueError(
+                f"SnailNum does not contain a pair at parent {idx}, left {left_idx}, right {right_idx}"
+            )
 
-#             left = num.get(num_left_idx)
-#             right = num.get(num_right_idx)
+    def magnitude(self):
+        return self._magnitude(0)
 
-#             if left is not None and right is not None:
-#                 result[result_idx] = EMPTY_NODE
-#                 _traverse(num, num_left_idx, result, result_left_idx)
-#                 _traverse(num, num_right_idx, result, result_right_idx)
-#             elif left is None and right is None:
-#                 result[result_idx] = num[num_idx]
-#             else:
-#                 raise ValueError(
-#                     f"SnailNum does not contain a pair at parent {num_idx}, left {num_left_idx}, right {num_right_idx}"
-#                 )
+    @classmethod
+    def _merge(cls, a, b):
+        # NOTE: Making this a heap made merging unnecessarily complex
+        result = SnailNum()
 
-#         _traverse(a, 0, result, 1)  # all a goes to result head's left
-#         _traverse(b, 0, result, 2)  # all b goes to result head's right
+        # TODO refactor to use foreach?
+        def _traverse(num, num_idx, result, result_idx):
+            num_left_idx = num.get_left_idx(num_idx)
+            num_right_idx = num.get_right_idx(num_idx)
 
-#         return result
+            result_left_idx = result.get_left_idx(result_idx)
+            result_right_idx = result.get_right_idx(result_idx)
 
-#     def _explode(self, idx):
-#         pass
-#         # left_idx, left = self.get_left(idx)
-#         # right_idx, right = self.get_right(idx)
+            left = num.get(num_left_idx)
+            right = num.get(num_right_idx)
 
-#         # def _traverse_up_and_to_left(idx, visited=set()):
-#         #     if idx <= 0:
-#         #         return None
+            if left is not None and right is not None:
+                result[result_idx] = EMPTY_NODE
+                _traverse(num, num_left_idx, result, result_left_idx)
+                _traverse(num, num_right_idx, result, result_right_idx)
+            elif left is None and right is None:
+                result[result_idx] = num[num_idx]
+            else:
+                raise ValueError(
+                    f"SnailNum does not contain a pair at parent {num_idx}, left {num_left_idx}, right {num_right_idx}"
+                )
 
-#         #     visited.add(idx)
+        _traverse(a, 0, result, 1)  # all a goes to result head's left
+        _traverse(b, 0, result, 2)  # all b goes to result head's right
 
-#         #     parent_idx = self.get_parent_idx(idx)
-#         #     left_idx = self.get_left_idx(idx)
-#         #     if left and left_idx not in visited:
-#         #         return left_idx
-#         #     else:
-#         #         return _traverse_up_and_to_left(parent_idx)
+        return result
 
-#         # def _traverse_up_and_to_right(idx, visited=set()):
-#         #     if idx <= 0:
-#         #         return None
+    def add(self, other):
+        result = SnailNum._merge(self, other)
+        # result._explode()
+        return result
 
-#         #     visited.add(idx)
+    def _explode(self, idx):
+        inorder_traversal = []
 
-#         #     parent_idx = self.get_parent_idx(idx)
-#         #     right_idx = self.get_right_idx(idx)
-#         #     if right_idx not in visited:
-#         #         return right_idx
-#         #     else:
-#         #         return _traverse_up_and_to_right(parent_idx)
+        def func(k, v):
+            inorder_traversal.append((k, v))
 
-#         # repl_left_idx = _traverse_up_and_to_left(left_idx)
-#         # if repl_left_idx:
-#         #     self[repl_left_idx] += left
+        self.foreach(func, SnailNum.ORDER_INORDER)
+        inorder_traversal = [k for k, v in inorder_traversal if type(v) == int]
 
-#         # repl_right_idx = _traverse_up_and_to_right(right_idx)
-#         # if repl_right_idx:
-#         #     self[repl_right_idx] += right
+        left_idx, left = self.get_left(idx)
+        right_idx, right = self.get_right(idx)
 
-#         # self[left_idx] = None
-#         # self[right_idx] = None
-#         # self[idx] = 0
+        # Scan list to find neighbors for each value, if exists
+        closest_left_search_idx = inorder_traversal.index(left_idx) - 1
+        closest_right_search_idx = inorder_traversal.index(right_idx) + 1
 
-#     def add(self, other):
-#         result = SnailNum._merge(self, other)
-#         # result._explode()
-#         return result
+        if closest_left_search_idx >= 0:
+            closest_left_idx = inorder_traversal[closest_left_search_idx]
+            self[closest_left_idx] += left
 
+        if closest_right_search_idx < len(inorder_traversal):
+            closest_right_idx = inorder_traversal[closest_right_search_idx]
+            self[closest_right_idx] += right
 
-# class SnailNumParser:
-#     @classmethod
-#     def parse(cls, str):
-#         # match all brackets, commas, and numbers
-#         pattern = r"([\[\]\,]|\d+)"
-
-#         snum = SnailNum()
-#         c_idx = 0
-
-#         for match in re.findall(pattern, str):
-#             if match == "[":
-#                 left_idx = snum.get_left_idx(c_idx)
-#                 left = snum.get(left_idx)
-#                 if not left:
-#                     snum[left_idx] = EMPTY_NODE
-#                 c_idx = left_idx
-#             elif match == "]":
-#                 c_idx = snum.get_parent_idx(c_idx)
-#                 pass
-#             elif match == ",":
-#                 c_idx = snum.get_parent_idx(c_idx)
-#                 right_idx = snum.get_right_idx(c_idx)
-#                 right = snum.get(right_idx)
-#                 if not right:
-#                     snum[right_idx] = EMPTY_NODE
-#                 c_idx = right_idx
-#             elif match.isdigit():
-#                 snum[c_idx] = int(match)
-
-#         return snum
+        # Set current node value to 0
+        self[idx] = 0
+        del self[left_idx]
+        del self[right_idx]
 
 
-# def _parse_input(lines):
-#     ## Start here
-#     snail_nums = [SnailNumParser.parse(line) for line in lines]
-#     return snail_nums
+class SnailNumParser:
+    @classmethod
+    def parse(cls, str):
+        # match all brackets, commas, and numbers
+        pattern = r"([\[\]\,]|\d+)"
+
+        snum = SnailNum()
+        c_idx = 0
+
+        for match in re.findall(pattern, str):
+            if match == "[":
+                left_idx = snum.get_left_idx(c_idx)
+                left = snum.get(left_idx)
+                if not left:
+                    snum[left_idx] = EMPTY_NODE
+                c_idx = left_idx
+            elif match == "]":
+                c_idx = snum.get_parent_idx(c_idx)
+                pass
+            elif match == ",":
+                c_idx = snum.get_parent_idx(c_idx)
+                right_idx = snum.get_right_idx(c_idx)
+                right = snum.get(right_idx)
+                if not right:
+                    snum[right_idx] = EMPTY_NODE
+                c_idx = right_idx
+            elif match.isdigit():
+                snum[c_idx] = int(match)
+
+        return snum
 
 
-# def part1():
-#     _ = _parse_input(auto_read_input())
-#     return None
+def _parse_input(lines):
+    ## Start here
+    snail_nums = [SnailNumParser.parse(line) for line in lines]
+    return snail_nums
 
 
-# def part2():
-#     _ = _parse_input(auto_read_input())
-#     return None
+def part1():
+    _ = _parse_input(auto_read_input())
+    return None
 
 
-# if __name__ == "__main__":
-#     print(part1())
-#     print(part2())
+def part2():
+    _ = _parse_input(auto_read_input())
+    return None
+
+
+if __name__ == "__main__":
+    print(part1())
+    print(part2())
