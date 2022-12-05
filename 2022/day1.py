ca5e87dc453e1f91ade8cfef63d5704033f6ab4d
JGMEYER <meyer.justian@gmail.com>
2022-12-05 18:32:47 -0500

    2022 1 Part 2

--- 2022/day1.py
+++ 2022/day1.py
@@ -17,14 +17,19 @@ def most_calories(snack_packs):
     return max([sum(pack) for pack in snack_packs])
 
 
+def top3_most_calories_total(snack_packs):
+    sorted_calories = sorted([sum(pack) for pack in snack_packs], reverse=True)
+    return sum(sorted_calories[:3])
+
+
 def part1():
     snack_packs = _parse_input(auto_read_input())
     return most_calories(snack_packs)
 
 
 def part2():
-    _ = _parse_input(auto_read_input())
-    return None
+    snack_packs = _parse_input(auto_read_input())
+    return top3_most_calories_total(snack_packs)
 
 
 if __name__ == "__main__":
--- 2022/tests/test_day1.py
+++ 2022/tests/test_day1.py
@@ -1,4 +1,4 @@
-from day1 import _parse_input, most_calories
+from day1 import _parse_input, most_calories, top3_most_calories_total
 from common.input import auto_read_input
 
 
@@ -11,6 +11,7 @@ def test__parse_input():
     assert snack_packs[3] == [7000, 8000, 9000]
     assert snack_packs[4] == [10000]
 
+
 def test_most_calories():
     snack_packs = [
         [1000, 2000, 3000],
@@ -21,3 +22,15 @@ def test_most_calories():
     ]
     expected_calories = 24000
     assert most_calories(snack_packs) == expected_calories
+
+
+def test_top3_most_calories_total():
+    snack_packs = [
+        [1000, 2000, 3000],
+        [4000],
+        [5000, 6000],
+        [7000, 8000, 9000],
+        [10000],
+    ]
+    expected_calories = 45000
+    assert top3_most_calories_total(snack_packs) == expected_calories
